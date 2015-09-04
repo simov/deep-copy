c703f060414c350d99cb28e54acaf5d97dcbfea2
simov <simeonvelichkov@gmail.com>
2015-09-04 11:11:53 +0300

    Update project files

--- README.md
+++ README.md
@@ -1,36 +1,19 @@
-deep-copy
-=======
 
-Deep copy any object or array.
+# deep-copy
 
-##Install
-Note that you can use this module with commonjs (nodejs), amdjs or as a global object.
+Deep copy objects and arrays
 
-```bash
-$ bower install deep-copy
-```
 
-or
+## Usage
 
-```bash
-$ npm install deep-copy
-```
-
-##Usage
-Pass in some object.
+Pass in some object
 
 ```js
 var copy = dcopy({a:{b:[{c:5}]}});
 ```
 
-Or an array.
+Or array
 
 ```js
 var copy = dcopy([1,2,{a:{b:5}}]);
 ```
-
-##Tests
-[http://simov.github.io/deep-copy/test/][1]
-
-
-  [1]: http://simov.github.io/deep-copy/test/
---
+++ test/index.js
@@ -0,0 +1,39 @@
+
+var should = require('should')
+var dcopy = require('../')
+
+
+describe('deep-copy', function () {
+  var obj = {
+    a: 0,
+    b: [0,1,2],
+    c: {
+      a: 0,
+      b: 1,
+    }
+  }
+  var arr = [
+    0,
+    1,
+    {
+      a: 0,
+      b: {
+        c: 1
+      }
+    }
+  ]
+
+  it('deep copy object', function () {
+    var copy = dcopy(obj)
+    copy.c = {c: 15}
+    should.deepEqual(obj.c, {a: 0, b: 1})
+    should.deepEqual(copy.c, {c: 15})
+  })
+
+  it('deep copy array', function () {
+    var copy = dcopy(arr)
+    copy[2].b = {d: 15}
+    should.deepEqual(arr[2].b, {c: 1})
+    should.deepEqual(copy[2].b, {d: 15})
+  })
+})
